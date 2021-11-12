850aa0ae426c02483e26d383d4b41343f8fbd05a
Duy Dang <duydangit24@gmail.com>
2021-11-13 00:11:14 +0700

    up mesage

--- bot.js
+++ bot.js
@@ -13,11 +13,25 @@ else {
 
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
-bot.on('message', (msg) => {
-  const name = msg.from.username;
-  bot.sendMessage(msg.chat.id, 'Xin chào anh/chị, ' + name + '!').then(() => {
-    // reply sent!
+bot.onText(/\/beba-xinchao (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const user_name = msg.from.username;
+  const chatId = msg.chat.id;
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, 'Bé ba chào anh, ' + user_name + '!').then(() => {
+        'test'
   });
 });
+
+// bot.on('message', (msg) => {
+//   const name = msg.from.username;
+//   bot.sendMessage(msg.chat.id, 'Xin chào anh/chị, ' + name + '!').then(() => {
+//     // reply sent!
+//   });
+// });
 
 module.exports = bot;
